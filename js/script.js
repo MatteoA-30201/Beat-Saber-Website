5525cd14101dbc562a36a3e9d7d558c762f3d684
MatteoA-30201 <125mamatucci@frhsd.com>
2025-03-17 09:47:41 -0400

    added photos for future usage

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// const app = Vue.createApp({
+//   data() {
+//     return {
+//       settings: [
+//         { name: 'Left', image: 'Photos/Left hand.JPG'},
+//         { name: 'Default', image: 'Photos/Default.JPG'},
+//         { name: 'Right', image: 'Photos/Right hand.JPG'},
+//       ],
+
+//     }
+//   },
+//   methods: {
+//     updateSettings(index) {
+//       this.settings = index
+//     }
+//   },
+//   computed: {
+//     image() {
+//       return this.settings[].image
+//     }
+//   }
+
+
+// })
