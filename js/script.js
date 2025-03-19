3dc6f06ea59ff9bb6178bbf97a85378c01e39863
MatteoA-30201 <125mamatucci@frhsd.com>
2025-03-19 09:03:17 -0400

    worked on javascript

--- js/script.js
+++ js/script.js
@@ -1,24 +1,28 @@
-// const app = Vue.createApp({
-//   data() {
-//     return {
-//       settings: [
-//         { name: 'Left', image: 'Photos/Left hand.JPG'},
-//         { name: 'Default', image: 'Photos/Default.JPG'},
-//         { name: 'Right', image: 'Photos/Right hand.JPG'},
-//       ],
+const vue_app = Vue.createApp({
+  data() {
+    return {
+      settings: [
+        { name: 'left-controller-container', image: 'Photos/Left hand.JPG'},
+        { name: 'default-controller-container', image: 'Photos/Default.JPG'},
+        { name: 'right-controller-container', image: 'Photos/Right hand.JPG'},
+      ],
+    }
+  },
+  methods: {
+    receiveEmit(settingsId) {
+      const elementId1 = document.getElementsByClassName(settings[0].name).id
+      const elementId2 = document.getElementsByClassName(settings[1].name).id;
+      const elementId3 = document.getElementsByClassName(settings[2].name).id;
+      console.log('Toggle settings:', settingsId);
+    }
+  },
+  // computed: {
+  //   image() {
+  //     return this.settings[index].image[0];
+  //   }
+  // }
 
-//     }
-//   },
-//   methods: {
-//     updateSettings(index) {
-//       this.settings = index
-//     }
-//   },
-//   computed: {
-//     image() {
-//       return this.settings[].image
-//     }
-//   }
 
+})
 
-// })
+vue_app.mount("#vue_app")
